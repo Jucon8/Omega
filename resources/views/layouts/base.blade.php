ea6982dfeb47ed4daa58f6fefa591496bcf6b238
Juan José Condinanzi <jucon8@gmail.com>
2020-11-18 11:46:36 -0300

    layout route fixing

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -158,10 +158,10 @@
 						<div class="container">
 							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
 								<li class="menu-item home-icon">
-									<a href="index.html" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+									<a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
 								</li>
 								<li class="menu-item">
-									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
+									<a href="/about" class="link-term mercado-item-title">About Us</a>
 								</li>
 								<li class="menu-item">
 									<a href="/shop" class="link-term mercado-item-title">Shop</a>
@@ -173,7 +173,7 @@
 									<a href="/checkout" class="link-term mercado-item-title">Checkout</a>
 								</li>
 								<li class="menu-item">
-									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
+									<a href="/contact" class="link-term mercado-item-title">Contact Us</a>
 								</li>																	
 							</ul>
 						</div>
